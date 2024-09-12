ad6b3dbde37b84a35d34496ab794b9e8a60c0b55
kaique818 <kaiquematheus142005@gmail.com>
2024-09-12 20:26:52 -0300

    criando janelas veyr

--- app/src/main/java/br/com/etecia/myapp/LoginActivity.java
+++ app/src/main/java/br/com/etecia/myapp/LoginActivity.java
@@ -1,5 +1,6 @@
 package br.com.etecia.myapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -39,7 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         btnCadastraSenha.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+              startActivity(new Intent(getApplicationContext(), CadastrarSenhaActivity.class));
             }
         });
     }
